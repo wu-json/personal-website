e89f657ecce068aae6101625b3f4d2db3b55abe5
Jason Wu <jason.wu2153@gmail.com>
2021-04-17 19:16:10 -0400

    added history

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/poppins/200.css';
 import { theme } from 'src/styles/theme';
 
-const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
+import { Navigation } from './Navigation';
+import { history } from './lib/history';
+
+const App = () => (
+    <ChakraProvider theme={theme}>
+        <Navigation history={history} />
+    </ChakraProvider>
+);
 
 export { App };
--- src/navigation.tsx
+++ src/navigation.tsx
@@ -2,7 +2,11 @@ import { History } from 'history';
 import { Route, Router, Switch } from 'react-router-dom';
 
 const Navigation = ({ history }: { history: History }) => (
-    <Router history={history}></Router>
+    <Router history={history}>
+        <Switch>
+            <Route path="/" />
+        </Switch>
+    </Router>
 );
 
 export { Navigation };
