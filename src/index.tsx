1f7f1fc13b5ceebdf974171c8b22e327c343ee9b
Jason Wu <jason.wu2153@gmail.com>
2021-04-17 19:50:26 -0400

    scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
-import './index.css';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
