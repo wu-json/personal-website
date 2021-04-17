d252cbebf34e82e329bdc5a8281bd74f1f0e89e9
Jason Wu <jason.wu2153@gmail.com>
2021-04-17 19:56:13 -0400

    nyan cat

--- src/screens/Home/sections/MainBanner/index.tsx
+++ src/screens/Home/sections/MainBanner/index.tsx
@@ -1,7 +1,19 @@
 import NyanCat from 'src/assets/graphics/nyan-cat.gif';
 
+import styles from './styles.module.scss';
+
 const MainBanner = () => {
-    return <div></div>;
+    return (
+        <div className={styles['page']}>
+            <div className={styles['container']}>
+                <img
+                    className={styles['nyan-cat']}
+                    src={NyanCat}
+                    alt="nyan-cat"
+                />
+            </div>
+        </div>
+    );
 };
 
 export { MainBanner };
