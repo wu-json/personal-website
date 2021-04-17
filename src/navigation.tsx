1a8cc3532308256fa3dc9f6a369bf665908970be
Jason Wu <jason.wu2153@gmail.com>
2021-04-17 19:17:19 -0400

    added home route

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,10 +1,11 @@
 import { History } from 'history';
 import { Route, Router, Switch } from 'react-router-dom';
+import { HomeScreen } from 'src/screens/Home';
 
 const Navigation = ({ history }: { history: History }) => (
     <Router history={history}>
         <Switch>
-            <Route path="/" />
+            <Route path="/" component={HomeScreen} />
         </Switch>
     </Router>
 );
