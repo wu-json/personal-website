7fd0c56bc39c70b15f7e3969414193e078da9070
Jason Wu <jason.wu2153@gmail.com>
2021-04-18 08:03:23 -0400

    added unstyled about me

--- src/screens/Home/sections/AboutMe/index.tsx
+++ src/screens/Home/sections/AboutMe/index.tsx
@@ -1,5 +1,16 @@
 import styles from './styles.module.scss';
 
-const AboutMe = () => <div></div>;
+const AboutMe = () => (
+    <div className={styles['page']}>
+        <div className={styles['container']}>
+            <h1>about me</h1>
+            <p>
+                I'm a junior at Yale University pursuing a B.S. in Computer
+                Science, and Full Stack Software Engineer at Snackpass. In my
+                free time, I like making LoFi music and reading about DeFi.
+            </p>
+        </div>
+    </div>
+);
 
 export { AboutMe };
