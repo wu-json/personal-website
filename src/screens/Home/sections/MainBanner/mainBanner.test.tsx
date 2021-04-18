395abbde71cc5251625d99cc990400d578cfda18
Jason Wu <jason.wu2153@gmail.com>
2021-04-18 11:09:48 -0400

    added social link tests

--- src/screens/Home/sections/MainBanner/mainBanner.test.tsx
+++ src/screens/Home/sections/MainBanner/mainBanner.test.tsx
@@ -3,11 +3,37 @@ import { render, screen } from '@testing-library/react';
 import { MainBanner } from './index';
 
 describe('Main Banner Tests', () => {
-    it('should link to Twitter', () => {
+    it('should link to correct socials', () => {
+        const testCases = [
+            {
+                alt: 'twitter',
+                expectedLink: 'https://twitter.com/wu_json'
+            },
+            {
+                alt: 'linkedin',
+                expectedLink: 'https://www.linkedin.com/in/jwu215/'
+            },
+            {
+                alt: 'github',
+                expectedLink: 'https://github.com/wu-json'
+            },
+            {
+                alt: 'email',
+                expectedLink: 'mailto:jason.c.wu@yale.edu'
+            }
+        ];
         render(<MainBanner />);
-        expect(screen.getByAltText('twitter').closest('a')).toHaveAttribute(
+        testCases.map(testCase => {
+            expect(
+                screen.getByAltText(testCase.alt).closest('a')
+            ).toHaveAttribute('href', testCase.expectedLink);
+        });
+    });
+    it('should link to resume', () => {
+        render(<MainBanner />);
+        expect(screen.getByText('resume').closest('a')).toHaveAttribute(
             'href',
-            'https://twitter.com/wu_json'
+            '/jasonwu-resume.pdf'
         );
     });
 });
