3f5f48fb490be6aa4d6e2de8ceaaf614235616d4
Jason Wu <jason.wu2153@gmail.com>
2021-04-18 09:04:58 -0400

    added content

---
+++ src/screens/Home/sections/Jobs/components/Job/index.tsx
@@ -0,0 +1,15 @@
+import { Job as JobType } from 'src/screens/Home/sections/Jobs/types';
+
+import styles from './styles.module.scss';
+
+const Job = ({ job }: { job: JobType }) => (
+    <div>
+        <img src={job.logo.src} alt={job.logo.alt} />
+        <div>
+            <h2>{job.companyName}</h2>
+            <h3>{`${job.title}, ${job.duration}`}</h3>
+        </div>
+    </div>
+);
+
+export { Job };
--- src/screens/Home/sections/Jobs/index.tsx
+++ src/screens/Home/sections/Jobs/index.tsx
@@ -1,9 +1,16 @@
+import { Job } from './components/Job';
+import { JOBS } from './constants';
 import styles from './styles.module.scss';
 
 const Jobs = () => (
     <div className={styles['wrapper']}>
         <div className={styles['container']}>
             <h1>where i've worked</h1>
+            <div>
+                {JOBS.map((job, i) => (
+                    <Job job={job} key={i} />
+                ))}
+            </div>
         </div>
     </div>
 );
