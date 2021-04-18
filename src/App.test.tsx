cc34e5513244c0a3dabe278359406777aa27ab96
Jason Wu <jason.wu2153@gmail.com>
2021-04-18 10:34:22 -0400

    turn off watchAll

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+import { App } from './App';
+
+describe('App Tests', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+});
