21251124a64994f63661984bd85a388d6d198289
Jason Wu <jason.wu2153@gmail.com>
2021-04-29 07:59:07 -0400

    added currently building section

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Footer } from 'src/components/Footer';
 
 import { AboutMe } from './sections/AboutMe';
+import { CurrentlyBuilding } from './sections/CurrentlyBuilding';
 import { Jobs } from './sections/Jobs';
 import { MainBanner } from './sections/MainBanner';
 import { Projects } from './sections/Projects';
@@ -12,6 +13,7 @@ const HomeScreen = () => {
             <MainBanner />
             <AboutMe />
             <Jobs />
+            <CurrentlyBuilding />
             <Projects />
             <Stack />
             <Footer />
