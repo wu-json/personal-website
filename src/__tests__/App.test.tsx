d904dfad19edd021aff059f1968ffb855e597fa9
wu-json <jason.wu2153@gmail.com>
2022-05-10 19:30:43 -0400

    feat: update testing libraries

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import { App } from '../App';
 
 describe('App Tests', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    const root = ReactDOM.createRoot(document.createElement('div'));
+    root.render(<App />);
   });
 });
