f818b5ce6a91a5bbf5a9cb84c576e0d824c84160
wu-json <jason.wu2153@gmail.com>
2023-07-09 16:27:40 -0400

    feat: add more projects

--- src/screens/Home/sections/Projects/constants.ts
+++ src/screens/Home/sections/Projects/constants.ts
@@ -1,9 +1,29 @@
+import ArimaLogo from 'src/assets/logos/arima.gif';
+import M1Logo from 'src/assets/logos/m1.png';
 import PathogenLogo from 'src/assets/logos/pathogen.png';
 import VailableLogo from 'src/assets/logos/vailable.png';
 
 import { Project } from './types';
 
 const PROJECTS: Project[] = [
+  {
+    logo: {
+      src: ArimaLogo,
+      alt: 'oshi-chan-logo',
+    },
+    projectName: 'oshi-chan',
+    desc: 'discord bot for new 9anime release notifications in rust',
+    href: 'https://github.com/wu-json/oshi-chan',
+  },
+  {
+    logo: {
+      src: M1Logo,
+      alt: 'm1-finance-data-viz-logo',
+    },
+    projectName: 'm1 finance data viz',
+    desc: 'm1 finance data visualizer in grafana + go',
+    href: 'https://github.com/wu-json/m1-finance-grafana',
+  },
   {
     logo: {
       src: VailableLogo,
