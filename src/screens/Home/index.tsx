afc78015855401d63516a20256e4043021e847c8
wu-json <jason.wu2153@gmail.com>
2023-12-03 20:47:36 -0500

    feat: replace pear

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,11 +5,9 @@ import { Jobs } from './sections/Jobs';
 import { MainBanner } from './sections/MainBanner';
 import { Projects } from './sections/Projects';
 import { Stack } from './sections/Stack';
-import { DarkPear } from './sections/MainBanner/DarkPear';
 
 const HomeScreen = () => (
   <>
-    <DarkPear />
     <MainBanner />
     <AboutMe />
     <Jobs />
