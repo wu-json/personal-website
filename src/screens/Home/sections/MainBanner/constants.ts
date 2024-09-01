d00b11ee72c3d62b7b92667158ae32683aa16d8b
wu-json <jason.wu2153@gmail.com>
2024-09-01 14:58:49 -0400

    feat: housekeeping

--- src/screens/Home/sections/MainBanner/constants.ts
+++ src/screens/Home/sections/MainBanner/constants.ts
@@ -1,18 +1,10 @@
 import EmailIcon from 'src/assets/icons/socials/email.png';
 import GitHubIcon from 'src/assets/icons/socials/github.png';
 import LinkedInIcon from 'src/assets/icons/socials/linkedin.png';
-import TwitterIcon from 'src/assets/icons/socials/twitter.png';
 
 import { Social } from './types';
 
 const SOCIALS: Social[] = [
-  {
-    icon: {
-      src: TwitterIcon,
-      alt: 'twitter',
-    },
-    href: 'https://twitter.com/wu_json',
-  },
   {
     icon: {
       src: LinkedInIcon,
