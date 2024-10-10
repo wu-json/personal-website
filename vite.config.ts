9dc814adab407203ac0430e536daed23504a7593
wu-json <jason.wu2153@gmail.com>
2024-10-09 20:37:12 -0700

    feat: config

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
