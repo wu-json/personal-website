bc309844bba022aceb266ae8d5386cd84e8cb9c7
wu-json <jason.wu2153@gmail.com>
2025-03-09 00:30:46 -0800

    feat: bio

--- src/screens/Home/sections/AboutMe/index.tsx
+++ src/screens/Home/sections/AboutMe/index.tsx
@@ -1,14 +1,14 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 const AboutMe = () => (
-  <div className={styles['wrapper']}>
-    <div className={styles['container']}>
+  <div className={styles.wrapper}>
+    <div className={styles.container}>
       <h1>about me</h1>
-      <div className={styles['text-wrapper']}>
+      <div className={styles["text-wrapper"]}>
         <p>
-          I'm a senior software engineer at Fizz, and Yale University graduate
-          (2022). These days, I primarily work on our backend APIs and durable
-          execution workflows with Temporal using TypeScript (I use Neovim btw).
+          I'm a founding engineer at Forge, and Yale University graduate (2022).
+          I like working with TypeScript, Neovim, observability, and durable
+          execution tools.
         </p>
       </div>
     </div>
--- src/screens/Home/sections/MainBanner/index.tsx
+++ src/screens/Home/sections/MainBanner/index.tsx
@@ -1,25 +1,25 @@
-import { DarkPear } from './DarkPear';
-import { SOCIALS } from './constants';
-import styles from './styles.module.scss';
+import { DarkPear } from "./DarkPear";
+import { SOCIALS } from "./constants";
+import styles from "./styles.module.scss";
 
 const MainBanner = () => (
-  <div className={styles['wrapper']}>
-    <div className={styles['container']}>
-      <div className={styles['profile']}>
+  <div className={styles.wrapper}>
+    <div className={styles.container}>
+      <div className={styles.profile}>
         <DarkPear />
       </div>
-      <div className={styles['text-container']}>
+      <div className={styles["text-container"]}>
         <h1>jason wu</h1>
-        <h2>software @fizz</h2>
-        <div className={styles['socials-container']}>
-          {SOCIALS.map((social, i) => (
-            <a href={social.href} key={i}>
+        <h2>software @forge</h2>
+        <div className={styles["socials-container"]}>
+          {SOCIALS.map((social) => (
+            <a href={social.href} key={social.href}>
               <img src={social.icon.src} alt={social.icon.alt} />
             </a>
           ))}
         </div>
-        <a className={styles['button-wrapper']} href='/jasonwu-resume.pdf'>
-          <button>resume</button>
+        <a className={styles["button-wrapper"]} href="/jasonwu-resume.pdf">
+          <button type="button">resume</button>
         </a>
       </div>
     </div>
