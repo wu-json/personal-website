f5a0f50e0401328f6129c51902d0c921861984b1
wu-json <jason.wu2153@gmail.com>
2025-12-27 12:49:05 -0500

    chore: use oxc toolchain

--- src/screens/Home/sections/Jobs/constants.ts
+++ src/screens/Home/sections/Jobs/constants.ts
@@ -1,72 +1,72 @@
-import AmazonLogo from "src/assets/logos/amazon.jpeg";
-import AtomFinanceLogo from "src/assets/logos/atomFinance.png";
-import FizzLogo from "src/assets/logos/fizz.jpg";
-import ForgeLogo from "src/assets/logos/forge.png";
-import RutterLogo from "src/assets/logos/rutter.jpeg";
-import SnackpassLogo from "src/assets/logos/snackpass.jpeg";
+import AmazonLogo from 'src/assets/logos/amazon.jpeg';
+import AtomFinanceLogo from 'src/assets/logos/atomFinance.png';
+import FizzLogo from 'src/assets/logos/fizz.jpg';
+import ForgeLogo from 'src/assets/logos/forge.png';
+import RutterLogo from 'src/assets/logos/rutter.jpeg';
+import SnackpassLogo from 'src/assets/logos/snackpass.jpeg';
 
-import type { Job } from "./types";
+import type { Job } from './types';
 
 const JOBS: Job[] = [
   {
     logo: {
       src: ForgeLogo,
-      alt: "forge-logo",
+      alt: 'forge-logo',
     },
-    companyName: "forge",
-    title: "founding engineer",
-    duration: "march 2025 - present",
-    href: "https://withforge.com/",
+    companyName: 'forge',
+    title: 'founding engineer',
+    duration: 'march 2025 - present',
+    href: 'https://withforge.com/',
   },
   {
     logo: {
       src: FizzLogo,
-      alt: "fizz-logo",
+      alt: 'fizz-logo',
     },
-    companyName: "fizz",
-    title: "head of eng",
-    duration: "feb 2024 - march 2025",
-    href: "https://joinfizz.com/",
+    companyName: 'fizz',
+    title: 'head of eng',
+    duration: 'feb 2024 - march 2025',
+    href: 'https://joinfizz.com/',
   },
   {
     logo: {
       src: SnackpassLogo,
-      alt: "snackpass-logo",
+      alt: 'snackpass-logo',
     },
-    companyName: "snackpass",
-    title: "software engineer",
-    duration: "may 2022 - feb 2024",
-    href: "https://partners.snackpass.co/",
+    companyName: 'snackpass',
+    title: 'software engineer',
+    duration: 'may 2022 - feb 2024',
+    href: 'https://partners.snackpass.co/',
   },
   {
     logo: {
       src: AtomFinanceLogo,
-      alt: "atom-finance-logo",
+      alt: 'atom-finance-logo',
     },
-    companyName: "atom finance",
-    title: "software engineer",
-    duration: "aug 2021 - may 2022",
-    href: "https://atom.finance/",
+    companyName: 'atom finance',
+    title: 'software engineer',
+    duration: 'aug 2021 - may 2022',
+    href: 'https://atom.finance/',
   },
   {
     logo: {
       src: RutterLogo,
-      alt: "rutter-logo",
+      alt: 'rutter-logo',
     },
-    companyName: "rutter",
-    title: "software engineer",
-    duration: "nov 2021 - april 2022",
-    href: "https://www.rutterapi.com/",
+    companyName: 'rutter',
+    title: 'software engineer',
+    duration: 'nov 2021 - april 2022',
+    href: 'https://www.rutterapi.com/',
   },
   {
     logo: {
       src: AmazonLogo,
-      alt: "amazon-logo",
+      alt: 'amazon-logo',
     },
-    companyName: "amazon",
-    title: "software engineer",
-    duration: "summer 2021",
-    href: "https://aws.amazon.com/",
+    companyName: 'amazon',
+    title: 'software engineer',
+    duration: 'summer 2021',
+    href: 'https://aws.amazon.com/',
   },
 ];
 
--- src/screens/Home/sections/MainBanner/index.tsx
+++ src/screens/Home/sections/MainBanner/index.tsx
@@ -1,6 +1,6 @@
-import { DarkPear } from "./DarkPear";
-import { SOCIALS } from "./constants";
-import styles from "./styles.module.scss";
+import { SOCIALS } from './constants';
+import { DarkPear } from './DarkPear';
+import styles from './styles.module.scss';
 
 const MainBanner = () => (
   <div className={styles.wrapper}>
@@ -8,18 +8,18 @@ const MainBanner = () => (
       <div className={styles.profile}>
         <DarkPear />
       </div>
-      <div className={styles["text-container"]}>
+      <div className={styles['text-container']}>
         <h1>jason wu</h1>
         <h2>software @forge</h2>
-        <div className={styles["socials-container"]}>
-          {SOCIALS.map((social) => (
+        <div className={styles['socials-container']}>
+          {SOCIALS.map(social => (
             <a href={social.href} key={social.href}>
               <img src={social.icon.src} alt={social.icon.alt} />
             </a>
           ))}
         </div>
-        <a className={styles["button-wrapper"]} href="/jasonwu-resume.pdf">
-          <button type="button">resume</button>
+        <a className={styles['button-wrapper']} href='/jasonwu-resume.pdf'>
+          <button type='button'>resume</button>
         </a>
       </div>
     </div>
