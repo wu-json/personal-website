18903e0b52e818f0a389df78617f65b154a9406b
wu-json <jason.wu2153@gmail.com>
2026-02-14 19:57:03 -0800

    feat: update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { HomeScreen } from 'src/screens/Home';
 
 const App = () => (
