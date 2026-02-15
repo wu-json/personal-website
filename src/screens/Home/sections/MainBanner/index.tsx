d2228bc26002e2440c7d562216dc6cb27ed3d4aa
wu-json <jason.wu2153@gmail.com>
2026-02-14 20:15:38 -0800

    feat: name

--- src/screens/Home/sections/MainBanner/index.tsx
+++ src/screens/Home/sections/MainBanner/index.tsx
@@ -1,6 +1,6 @@
 const MainBanner = () => (
   <div className='w-full h-screen bg-black flex items-center justify-center'>
-    <h1 className='text-white text-6xl font-bold'>Hello World</h1>
+    <h1 className='text-white text-6xl font-pixel'>jason wu</h1>
   </div>
 );
 
