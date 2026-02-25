057f02601dec8ae0cf18d651be974ddb58d19441
wu-json <jason.wu2153@gmail.com>
2026-02-25 11:44:15 -0800

    fix: flash

--- src/App.tsx
+++ src/App.tsx
@@ -13,32 +13,36 @@ import { TransmissionDetail } from 'src/screens/Transmissions/TransmissionDetail
 import { Route, Switch } from 'wouter';
 
 const App = () => (
-  <RootLayout>
-    <Switch>
-      <Route path='/' component={HomeScreen} />
-      <Route path='/memories' component={MemoriesScreen} />
-      <Route path='/memories/:id'>
-        {params => <FragmentDetail id={params.id} />}
-      </Route>
-      <Route path='/memories/:id/:photo'>
-        {params => <FragmentDetail id={params.id} photo={params.photo} />}
-      </Route>
-      <Route path='/transmissions' component={TransmissionsScreen} />
-      <Route path='/transmissions/:id'>
-        {params => <TransmissionDetail id={params.id} />}
-      </Route>
-      <Route path='/constructs' component={ConstructsScreen} />
-      <Route path='/constructs/:id'>
-        {params => <ConstructDetail id={params.id} />}
-      </Route>
-      <Route path='/heroes' component={HeroesScreen} />
-      <Route path='/heroes/:id'>
-        {params => <HeroDetail id={params.id} />}
-      </Route>
-      <Route path='/gallery' component={GalleryScreen} />
-      <Route path='/garden' component={GardenScreen} />
-    </Switch>
-  </RootLayout>
+  <Switch>
+    <Route path='/gallery' component={GalleryScreen} />
+    <Route>
+      <RootLayout>
+        <Switch>
+          <Route path='/' component={HomeScreen} />
+          <Route path='/memories' component={MemoriesScreen} />
+          <Route path='/memories/:id'>
+            {params => <FragmentDetail id={params.id} />}
+          </Route>
+          <Route path='/memories/:id/:photo'>
+            {params => <FragmentDetail id={params.id} photo={params.photo} />}
+          </Route>
+          <Route path='/transmissions' component={TransmissionsScreen} />
+          <Route path='/transmissions/:id'>
+            {params => <TransmissionDetail id={params.id} />}
+          </Route>
+          <Route path='/constructs' component={ConstructsScreen} />
+          <Route path='/constructs/:id'>
+            {params => <ConstructDetail id={params.id} />}
+          </Route>
+          <Route path='/heroes' component={HeroesScreen} />
+          <Route path='/heroes/:id'>
+            {params => <HeroDetail id={params.id} />}
+          </Route>
+          <Route path='/garden' component={GardenScreen} />
+        </Switch>
+      </RootLayout>
+    </Route>
+  </Switch>
 );
 
 export { App };
